9e86e457d4d11dfa804d00eb65abae963a353057
riad-sorker <smriadhosen@gmail.com>
2026-09-22 12:55:41 +0600

    index.html, style.css , script.js and tailwind.css file has been created

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+
+const mbi = document.getElementById("mbi");
+const navlinks = document.getElementById("nav-links");
+
+mbi.addEventListener("click", () => {
+    navlinks.classList.toggle("active");
+})
